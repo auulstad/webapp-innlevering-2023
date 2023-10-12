8f358379f1ceaa073cde18e561d435ff346580e1
auulstad <vilde.aulstad@gmail.com>
2023-10-12 12:59:19 +0200

    vet ikke?

---
+++ testing-23/quiz.test.js
@@ -0,0 +1,28 @@
+const quizData = require('./questions.json');
+
+describe('Quiz Questions', () => {
+  quizData.questions.forEach((question, index) => {
+    describe(`Question ${index + 1}: ${question.title}`, () => {
+      test('answers is an array with exactly four items', () => {
+        expect(Array.isArray(question.answers)).toBe(true);
+        expect(question.answers.length).toBe(4);
+      });
+
+      it('should have a correct answer', () => {
+        const correctAnswers = question.answers.filter((answer) => answer.correct === true);
+        expect(correctAnswers).toHaveLength(1);
+      });
+
+      it('should have a helper text', () => {
+        expect(question.helper).toBeDefined();
+        expect(question.helper.text).toBeTruthy();
+      });
+
+      it('should have answer options with non-empty text', () => {
+        question.answers.forEach((answer, answerIndex) => {
+          expect(answer.answer).toBeTruthy();
+        });
+      });
+    });
+  });
+});
